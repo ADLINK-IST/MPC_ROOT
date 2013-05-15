5f865f6db9d50b1932a80c17522f52f972ed0ead
Philip Conroy <pac_at_prismtech.com>
2013-05-15 11:21:07 +0100

    OSPL-1606 added name modifier to wb30projects.lst filename

--- modules/WB30WorkspaceCreator.pm
+++ modules/WB30WorkspaceCreator.pm
@@ -35,7 +35,18 @@ sub get_additional_output {
   ## Create the accompanying list file.  It always goes in the same
   ## directory as the first workspace output file.  See
   ## WorkspaceCreator.pm for a description of the array elements.
-  return [[undef, 'wb30projects.lst', \&WB26WorkspaceCreator::list_file_body]];
+  my($self) = @_;
+  my $name = 'wb30projects';
+  if ($self->get_apply_project()) {
+     my $nmod = $self->get_name_modifier();
+     if (defined $nmod) {
+        $nmod =~ s/\*/$name/g;
+        $name = $nmod;
+     }
+  }
+  $name = $name . '.lst';
+
+  return [[undef, "$name", \&WB26WorkspaceCreator::list_file_body]];
 }
 
 
